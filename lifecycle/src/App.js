00f283b858c3e342f763dab3dceebdd5624cb167
gonulgulsen <gonulgulsenkurt@gmail.com>
2025-05-29 21:17:10 +0300

    useEffect

---
+++ lifecycle/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+function App() {
+const [number, setNumber] = useState(0);
+const [name, setName] = useState('Berker')
+// Burada herhangi bir state güncellendiği anda console da State güncellendi yazar
+// useEffect, useState gibi hooklar if içerisinde kullanılmaz
+useEffect(() => {
+  console.log("State güncellendi")
+})
+// Buradaki [] dependency array diye adlandırılır.Eğer içine bir şey yazmazsak component mount edildiği anı yakalarsın.
+
+useEffect(() => {
+  console.log("Component mount edildi.")
+}, [])
+
+//Burada ise number her güncellendiği anı yakalarsın
+useEffect(() => {
+  console.log("number state güncellendi")
+}, [number])
+
+// array içerisine istenilen kadar state eklenebilir.
+useEffect(() => {
+  console.log("number veya name state güncellendi")
+}, [number, name])
+
+  return (
+    <div className="App">
+      <h1>{number}</h1>
+      <button onClick= {() => setNumber(number + 1)}>Click</button>
+      <hr/>
+      <h1>{name}</h1>
+      <button onClick= {() => setName("Gönül")}>Click</button>
+    </div>
+  );
+}
+
+export default App;
