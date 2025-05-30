5e157030f6bce61a9f98e5afc670c39f803143f3
gonulgulsen <gonulgulsenkurt@gmail.com>
2025-05-30 16:32:13 +0300

    style definitions

---
+++ style-definitions/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import A from "./components/A";
+import B from "./components/B";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div style={{color: "red", backgroundColor: "white", paddingTop: 50}}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+
+        <div class="alert alert-danger" role="alert">
+         A simple danger alert—check it out!
+        </div>
+
+        <A/>
+        <B/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ style-definitions/src/components/A/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+function A() {
+  return (
+    <div className={styles.title}>
+      A
+    </div>
+  )
+}
+
+export default A
