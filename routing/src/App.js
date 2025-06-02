5b0e831274130e2c65431d82cd75015ae4c0a82e
gonulgulsen <gonulgulsenkurt@gmail.com>
2025-06-02 16:02:55 +0300

    url paramaters-basic router

---
+++ routing/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import Home from './components/Home';
+import Users from './components/Users';
+import About from './components/About';
+import User from './components/User';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} /> 
+          <Route path="/user/:id" element={ <User/>} />       
+        </Routes>
+      </div>
+    </Router>
+
+  );
+
+  }
+
+export default App;
