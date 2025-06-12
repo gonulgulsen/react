0c60c759f00cb06dd64fc1fa99b89383201f1ec8
gonulgulsen <gonulgulsenkurt@gmail.com>
2025-06-12 14:14:03 +0300

    install formik

---
+++ formik/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { Formik, Field, Form } from 'formik'; 
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Sign Up</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      onSubmit={(values) =>{ console.log(values)}}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" placeholder="Jane" />
+        {/* Burada initialValues daki değerin ismi ile name aynı olmalı */}
+          <br/>
+          <br/>
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" placeholder="Doe" />
+        <br/>
+        <br/>
+        <label htmlFor="email">Email</label>
+        <Field
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+        />
+        <br/>
+        <br/>
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+    </div>
+  );
+}
+
+export default App;
