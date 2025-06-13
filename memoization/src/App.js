46e46d350154f2d37856bf18e127edc44392aac6
gonulgulsen <gonulgulsenkurt@gmail.com>
2025-06-13 14:51:36 +0300

    useCallback

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -1,54 +1,37 @@
 import './App.css';
 import Header from './components/Header';
-import {useState, useMemo} from 'react'
+import {useState, useMemo, useCallback} from 'react'
 
 function App() {
 
   const [number, setNumber] = useState(0);
   const [text, setText] = useState("");
 
-  //  Burada data prop unu useMemo ile kullanmazsak Header re-render edilir
-  // const data = useMemo(()=>{
-  //   return {name: "Gönül"};
-  // },[])
-
-
-
-  //   const data = useMemo(()=>{
-  //   return [{name: "Gönül" , number}];
+  // const increment = useCallback(() => {
+  //    setNumber(number + 1)
   // },[number])
-//  Dependency array değiştiğinde artık sadece data hesaplanır
+  // Yukarıdaki gibi kullanırsak useCallback hook da sürekli number değişeceğinden header re-render edilir.
 
 
-
-    const data = useMemo(()=>{
-    return calculateObject();
-    },[])
-    // Bu şekilde yazınca calculateObject fonksiyondaki loglara girmedi
-
-
-
-    // const data = calculateObject();
-    // Bu yöntemle sayfa yavaşlar. 
+  const increment = useCallback(() => {
+     setNumber((prevState) => prevState + 1)
+  },[])
+  //Bu şekilde kullanılırsa Header re-render olmaz, prevstate ile bir önceki state i almış olduk
 
   return (
     <div className="App">
-     <Header number={number < 5 ? 0 : number} data={data}/>
+     <Header increment={increment} />
+
      <hr/>
+
      <h1>{number}</h1>
-     <button onClick={() => setNumber(number +1)}>Click</button>
+
      <br/>
      <br/>
+
      <input type={text} onChange={({target})=> setText(target.value)}/>
     </div>
   );
 }
 
-
-function calculateObject(){
-  console.log("Calculating...");
-  for(let i=0; i < 10000000; i++){}
-  console.log("Calculating completed!");
-  return {name: "Gülsen"}
-}
 export default App;
--- memoization/src/components/Header.js
+++ memoization/src/components/Header.js
@@ -1,16 +1,29 @@
 import React from 'react'
 
-function Header({number, data}) {
+function Header({increment}) {
   console.log("Header component R-rendered!")
   return (
     <div>
-      Header - {number}
+      Header
+
       <br/>
       <br/>
 
-      <code>{JSON.stringify(data)}</code>
+      <button onClick={increment}>Click</button>
     </div>
   )
 }
 // React.memo ile sarmalanan component yeni prop alıp ya da prop değişmediği sürece re-render olmaz
+
+// useMemo herhangi bir componente geçilen propslardaki değişim esnasında oluşacak gereksiz renderi engellemek için kullanılır 
+// veya component içinde iki farklı değer varsa
+// ve bu değerler referans tipliyse yani object,array gibi tiplerse o zaman referans adresleri her re-render olduğunda değişecek 
+// ve gereksiz re-render olacaktır.
+// Bu yüzden çalışmasını istemediğimiz bir şeyi useMemo içinde yazmamız lazım.... 
+// useCallBack ise herhangi bir componente gönderilen fonksiyonun baştan tanımlanmaması için yani re-render olmaması için kullanılır....
+// React.memo'nun ise iki farklı açıklaması var. 
+// Birincisi componente props veya fonksiyon geçilmemişse
+// ve gereksiz render oluyorsa render olmasını istemediğimiz componentin export default kısmında
+// React.memo koymamız gerekir.
+// İkincisi ise componente geçilen props değişince render olmasını sağlayabiliriz
 export default React.memo(Header)
