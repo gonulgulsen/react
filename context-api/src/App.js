21d30d781746d2dc4c8aace7ac5e8b380107137b
gonulgulsen <gonulgulsenkurt@gmail.com>
2025-06-16 11:10:00 +0300

    context theme switcher

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Button from './components/Button';
-import Header from './components/Header';
+import Container from './components/Container';
 import {ThemeProvider} from './context/ThemeContext';
 
 function App() {
   return (
-   <ThemeProvider value="dark">
-    <Header/>
-    <hr/>
-    <Button/>
+   <ThemeProvider>
+     <Container/>
    </ThemeProvider>
   );
 }
