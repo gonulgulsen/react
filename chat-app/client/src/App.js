1d94aa351430de450a5e220d3f2eda3b051c30b0
gonulgulsen <gonulgulsenkurt@gmail.com>
2025-06-23 16:08:52 +0300

    chat-app

---
+++ chat-app/client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import { ChatProvider } from './context/ChatContext';
+import Container from './components/Container';
+
+
+function App() {
+  return (
+  <ChatProvider>
+    <Container/>
+  </ChatProvider>
+  )
+
+}
+
+export default App;
---
+++ chat-app/client/src/components/ChatList.js
@@ -0,0 +1,24 @@
+import styles from "./style.module.css"
+
+import { useChat } from "../context/ChatContext"
+import ChatItem from "./ChatItem";
+
+import ScrollableFeed from 'react-scrollable-feed'
+
+
+function ChatList() {
+  const { messages } = useChat();
+
+  return (
+    <div className={styles.chatlist}>
+        <ScrollableFeed forceScroll={true}>
+            { messages.map((item, key) => (
+            <ChatItem key={key} item={item}/>
+             ))}
+        </ScrollableFeed>
+      
+    </div>
+  )
+}
+
+export default ChatList
